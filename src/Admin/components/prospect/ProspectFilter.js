f640eb712f1626c99c105b1aa2c0a9b9c9ae0f6f
SohaibArbiBakcha <bakchalona@gmail.com>
2021-09-07 19:28:02 +0100

    add filter to  prospect by cities ☺

--- src/Admin/components/prospect/ProspectFilter.js
+++ src/Admin/components/prospect/ProspectFilter.js
@@ -4,26 +4,30 @@ import {
   TextInput,
   SelectArrayInput,
   ReferenceArrayInput,
+  ReferenceInput,
 } from "ra-ui-materialui";
 
 const ProspectFilter = (props) => (
   <Filter {...props}>
-    <TextInput label="Name" source="name" />
-    <TextInput label="Email" source="email" />
-    <TextInput label="Phone Number" source="phoneNumber" />
-    <TextInput label="Comment" source="comment" />
+    <TextInput label='Name' source='name' />
+    <TextInput label='Email' source='email' />
+    <TextInput label='Phone Number' source='phoneNumber' />
+    <TextInput label='Comment' source='comment' />
     <SelectInput
-      label="Status"
-      source="statu"
+      label='Status'
+      source='statu'
       choices={[
         { id: true, name: "True" },
         { id: false, name: "False" },
       ]}
       allowEmpty
     />
-    <ReferenceArrayInput source="cours" reference="courses">
-      <SelectArrayInput optionText="name" />
+    <ReferenceArrayInput source='cours' reference='courses'>
+      <SelectArrayInput optionText='name' />
     </ReferenceArrayInput>
+    <ReferenceInput source='city' reference='cities'>
+      <SelectInput optionText='name' />
+    </ReferenceInput>
   </Filter>
 );
 export default ProspectFilter;
