ee8b09eed54a6900181d7ee627d66bf7e745d960
SohaibArbiBakcha <bakchalona@gmail.com>
2021-09-19 21:29:53 +0100

    remove student from filter status in prospect

--- src/Admin/components/prospect/ProspectFilter.js
+++ src/Admin/components/prospect/ProspectFilter.js
@@ -10,28 +10,27 @@ import {
 
 const ProspectFilter = (props) => (
   <Filter {...props}>
-    <TextInput label="Name" source="name" />
-    <TextInput label="Email" source="email" />
-    <TextInput label="Phone Number" source="phoneNumber" />
-    <TextInput label="Comment" source="comment" />
+    <TextInput label='Name' source='name' />
+    <TextInput label='Email' source='email' />
+    <TextInput label='Phone Number' source='phoneNumber' />
+    <TextInput label='Comment' source='comment' />
     <SelectInput
-      label="Status"
-      source="status"
+      label='Status'
+      source='status'
       choices={[
         { id: "pending", name: "Pending" },
         { id: "treated", name: "Treated" },
-        { id: "student", name: "Student" },
       ]}
       allowEmpty
       alwaysOn
     />
-    <ReferenceArrayInput source="cours" reference="courses">
-      <SelectArrayInput optionText="name" />
+    <ReferenceArrayInput source='cours' reference='courses'>
+      <SelectArrayInput optionText='name' />
     </ReferenceArrayInput>
-    <ReferenceInput source="city" label="City" reference="cities">
-      <SelectInput optionText="name" />
+    <ReferenceInput source='city' label='City' reference='cities'>
+      <SelectInput optionText='name' />
     </ReferenceInput>
-    <DateInput source="RegisteredAt" />
+    <DateInput source='RegisteredAt' />
   </Filter>
 );
 export default ProspectFilter;
